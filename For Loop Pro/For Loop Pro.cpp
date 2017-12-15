d13fd62566cd0d54037d5f668d6d773ddfd67c1c
ihsan <ihsanmpm@gmail.com>
2017-12-15 21:01:32 +0530

    Add project files.

---
+++ For Loop Pro/For Loop Pro.cpp
@@ -0,0 +1,27 @@
+// For Loop Pro.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	string str[] = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+	long long a, b;
+	cin >> a >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		if (i <= 9) cout << str[i] << endl;
+		else
+		{
+			if (!(i % 2)) cout << "even" << endl;
+			else cout << "odd" << endl;
+		}
+	}
+
+    return 0;
+}
+
